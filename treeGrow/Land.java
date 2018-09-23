182ab1a112d3a56ca0851dbc02eb24442b87d6e1
Josh <josh.e.stein@gmail.com>
2018-09-23 15:41:40 +0200

    Updated getter and setters

--- treeGrow/Land.java
+++ treeGrow/Land.java
@@ -2,47 +2,51 @@ package treeGrow;
 
 public class Land{
 
+	float[][] grid;
+	int[][] original_grid;
+	float[][] shade;
+
 	// to do
 	// sun exposure data here
 
 	static float shadefraction = 0.1f; // only this fraction of light is transmitted by a tree
 
 	Land(int dx, int dy) {
-		// to do
+		grid = new int[dx][dy];
+		shade = new int[dx][dy];
 	}
 
 	int getDimX() {
-		// to do
-		return 0; // incorrect value
+		return grid[0].length;
 	}
 
 	int getDimY() {
-		// to do
-		return 0; // incorrect value
+		return grid[1].length;
 	}
 
 	// Reset the shaded landscape to the same as the initial sun exposed landscape
 	// Needs to be done after each growth pass of the simulator
 	void resetShade() {
-		// to do
+		//TODO: test that this works.
+		shade = new int[dx][dy];
 	}
 
 	float getFull(int x, int y) {
-		// to do
-		return 0.0f; // incorrect value
+		return grid[x][y];
 	}
 
 	void setFull(int x, int y, float val) {
 		// to do
+		grid[x][y] = val;
 	}
 
 	float getShade(int x, int y) {
 		// to do
-		return 0.0f; // incorrect value
+		return shade[x][y];
 	}
 
 	void setShade(int x, int y, float val){
-		// to do
+		shade[x][y] = val;
 	}
 
 	// reduce the
